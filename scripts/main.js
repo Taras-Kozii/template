f0bf60cf388f23611a461b2fcdb44bd1dde5fb45
Taras Kozii <103899071+Taras-Kozii@users.noreply.github.com>
2025-08-18 13:48:07 +0000

    start

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,12 @@
 'use strict';
 
-import { initSlider } from './sliders.js';
 import { initNavigation } from './menu.js';
-import { initPopups } from './popups.js';
-import { initForms } from './forms.js';
 import { initHeader } from './header.js';
-import { initImageTranslate } from './hover-efects.js';
-import { watcherAnim, watcherToggle, countAnimate } from './animation.js';
+// import { watcherAnim, watcherToggle, countAnimate } from './animation.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-  initSlider();
   initNavigation();
-  initPopups();
-  initForms();
-  initImageTranslate();
   initHeader();
-
-  watcherToggle('.hero .scroll-anim', 0.3);
-  watcherAnim('[data-counter]', 0.2, countAnimate, false);
-  watcherToggle('.hero .scroll-anim', 0.3);
-  watcherToggle('.section-header.scroll-anim', 0.9);
-  watcherToggle('.map .scroll-anim', 0.3);
-  watcherToggle('.get-info .scroll-anim', 0.3);
+  // watcherToggle('.hero .scroll-anim', 0.3);
+  // watcherAnim('[data-counter]', 0.2, countAnimate, false);
 });
